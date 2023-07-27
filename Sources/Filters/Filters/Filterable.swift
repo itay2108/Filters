9b8edac58f8b7a850bcafd483457cf57470c1541
Itay Gervash <gervash@icloud.com>
2023-07-27 13:28:06 +0300

    add image and color methods for filterable, add minimum iOS version 13

--- Sources/Filters/Filters/Filterable.swift
+++ Sources/Filters/Filters/Filterable.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftUI
 
 /// Represents a type that can be filtered by a FilterPerformable. Must conform to FilterKeyPAthRepresentable.
 public protocol Filterable: FilterKeyPathRepresentable {
@@ -6,6 +7,8 @@ public protocol Filterable: FilterKeyPathRepresentable {
     static func keyDisplayName<T: FilterPerformable>(for filter: T) -> String
     static func valueDisplayName<T: FilterPerformable>(for filter: T, value filterValue: AnyEquatable) -> String
     static func displayPriotity<T: FilterPerformable>(for filter: T) -> FilterDisplayPriority
+    static func displayImageName<T: FilterPerformable>(for filter: T, value filterValue: AnyEquatable) -> String?
+    static func displayColor<T: FilterPerformable>(for filter: T, value filterValue: AnyEquatable) -> Color?
 }
 
 public extension Filterable {
@@ -22,6 +25,13 @@ public extension Filterable {
         return .normal
     }
     
+    static func displayImageName<T: FilterPerformable>(for filter: T, value filterValue: AnyEquatable) -> String? {
+        return nil
+    }
+    
+    static func displayColor<T: FilterPerformable>(for filter: T, value filterValue: AnyEquatable) -> Color? {
+        return nil
+    }
 }
 
 extension Array where Element: Filterable {
